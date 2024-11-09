daff74f52445dcecfa54a2b5f0a72626efd7dc3a
andreduarte99 <andre.duarte16@outlook.com>
2024-11-09 13:52:14 -0300

    finalizando parte 2

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,16 +12,28 @@ module.exports = {
         'fundo-card': 'var(--fundo-card)',
         'link-primario': 'var(--link-primario)',
         'escuro': 'var(--texto-escuro)',
+        'input': 'var(--input)',
       },
       width: {
         '1560': '1560px',
         '780': '780px',
         '160': '160px',
+        '34': '34px',
+  
       },
       height: {
         '468': '468px',
         '238': '238px',
         '160': '160px',
+        '34': '34px',
+
+      },
+      padding: {
+        '28': '28rem',
+      },
+      spacing: {
+        '94': '23rem',
+        '25': '7rem',
       },
     },
   },
